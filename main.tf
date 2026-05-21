84c78ee25e3510d168badf710d4336d69146b2f8
Cooper-Cooperson <187382869+Cooper-Cooperson@users.noreply.github.com>
2026-05-21 11:43:45 +0200

    added provider login

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_version = ">= 1.6.0"
 
+  required_providers {
+    ovh = {
+      source = "ovh/ovh"
+    }
+  }
+}
+
+provider "ovh" {
+  endpoint           = var.endpoint
+  application_key    = var.application_key
+  application_secret = var.application_secret
+  consumer_key       = var.consumer_key
+}
