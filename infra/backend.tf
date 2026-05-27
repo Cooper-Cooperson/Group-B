87c4a1d123167bf1ac8da7e1442fbda1ad93bf5c
Brian <i546779@fontysict.nl>
2026-05-27 11:35:40 +0200

    fixed endpoint config

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-group-b"
     region         = "de1" 
     key            = "infra/terraform.tfstate"
-    endpoint       = "https://s3.de1.io.cloud.ovh.net"
+    endpoints = {s3 = "https://s3.de1.cloud.ovh.net"}
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
--- keycloak/backend.tf
+++ keycloak/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-group-b"
     region         = "de1" 
-    endpoint       = "https://s3.de1.io.cloud.ovh.net"
     key            = "keycloak/terraform.tfstate"
+    endpoints = {s3 = "https://s3.de1.cloud.ovh.net"}
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
