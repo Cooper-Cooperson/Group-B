e76c9c7a5410a308fa33afd245a9f8e1b4625956
Cooper-Cooperson <187382869+Cooper-Cooperson@users.noreply.github.com>
2026-05-27 11:03:06 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "swift" {
-    container         = "terraform-state"
-    archive_container = "terraform-state"
+    container         = "terraform-state-group-b"
+    archive_container = "terraform-state-group-b"
     region            = "DE1" 
     auth_url          = "https://auth.cloud.ovh.net/v3"
+    key               = "infra/terraform.tfstate"
   }
 }
