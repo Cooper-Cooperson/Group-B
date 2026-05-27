8bd5d4362687f9f48eb50ef1bbb883de10bbb1ae
Brian <i546779@fontysict.nl>
2026-05-27 11:31:25 +0200

    fixed backend to use s3 like bucket instead of swift wich was unsuported by terraform

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,12 @@
 terraform {
-  backend "swift" {
-    container         = "terraform-state"
-    archive_container = "terraform-state"
-    region            = "DE1" 
-    auth_url          = "https://auth.cloud.ovh.net/v3"
-    key               = "infra/terraform.tfstate"
+  backend "s3" {
+    bucket         = "terraform-state-group-b"
+    region         = "de1" 
+    key            = "infra/terraform.tfstate"
+    endpoint       = "https://s3.de1.io.cloud.ovh.net"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
   }
 }
