9b4d9ae693ed849302c500adf8c6fb68626b9b6e
Brian <i546779@fontysict.nl>
2026-05-29 14:17:50 +0200

    move keycloack infra to keycloack map

--- infra/main.tf
+++ infra/main.tf
@@ -127,7 +127,7 @@ Enable the IP Firewall on the server's public IP
 
 resource "ovh_ip_firewall" "server" {
   depends_on = [ovh_cloud_project_instance.server]
-  
+
   ip             = local.public_ipv4
   ip_on_firewall = local.public_ipv4
   enabled        = true
@@ -357,71 +357,3 @@ data "ovh_domain_zone" "main" {
   name = var.domain
 }
 */
-
-/*
-Private network
-*/
-
-resource "ovh_cloud_project_network_private" "network" {
-  service_name = var.ovh_project_id 
-  vlan_id     = 42
-  name       = "terraform_testacc_private_net"
-  regions    = [var.region]
-}
-
-resource "ovh_cloud_project_network_private_subnet" "subnet" {
-  service_name = var.ovh_project_id 
-  network_id   = ovh_cloud_project_network_private.network.id
-
-  region     = var.region
-  start      = "192.168.168.100"
-  end        = "192.168.168.200"
-  network    = "192.168.168.0/24"
-  dhcp       = true
-  no_gateway = false
-}
-
-resource "ovh_cloud_project_gateway" "gateway" {
-  service_name = var.ovh_project_id 
-  name       = "gateway"
-  model      = "s"
-  region     = var.region
-  network_id = tolist(ovh_cloud_project_network_private.network.regions_attributes[*].openstackid)[0]
-  subnet_id  = ovh_cloud_project_network_private_subnet.subnet.id
-}
-
-data "template_file" "cloud_init" {
-  template = file("${path.module}/cloud-init-keycloak-caddy.yaml.tpl")
-
-  vars = {
-    KEYCLOAK_VERSION   = var.keycloak_version
-    KEYCLOAK_ADMIN     = var.keycloak_admin_user
-    KEYCLOAK_PASSWORD  = var.keycloak_admin_password
-    KEYCLOAK_HOSTNAME  = var.keycloak_hostname
-    LETSENCRYPT_EMAIL  = "admin@${replace(var.keycloak_hostname, "/^[^.]+\\./", "")}"
-  }
-}
-
-resource "ovh_cloud_project_instance" "keycloak_vm" {
-  service_name = var.ovh_project_id
-  region       = var.region
-  name         = "keycloak-vm"
-  
-  flavor {
-    flavor_id = var.instance_flavor
-  }
-
-  boot_from {
-    image_id = var.instance_image
-  }
-
-  ssh_key {
-    name = var.ssh_key_name_keycloack
-  }
-  billing_period = "hourly"
-  user_data       = data.template_file.cloud_init.rendered
-
-  network {
-    public = true
-  }
-}
--- keycloak/main.tf
+++ keycloak/main.tf
@@ -1,50 +1,85 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
-    keycloak = {
-      source  = "keycloak/keycloak"
-      version = "~> 5.0"
+    ovh = {
+      source = "ovh/ovh"
+      version = ">= 2.1.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.2.0"
     }
   }
 }
 
-provider "keycloak" {
-  url       = var.keycloak_url
-  realm     = "master"
-  client_id = "admin-cli"
-  username  = var.keycloak_admin_user
-  password  = var.keycloak_admin_password
+provider "ovh" {
+  endpoint           = var.endpoint
+  application_key    = var.application_key
+  application_secret = var.application_secret
+  consumer_key       = var.consumer_key
 }
 
-resource "keycloak_realm" "apps" {
-  realm        = "apps"
-  enabled      = true
-  display_name = "Apps Realm"
+resource "ovh_cloud_project_network_private" "network" {
+  service_name = var.ovh_project_id 
+  vlan_id     = 42
+  name       = "terraform_testacc_private_net"
+  regions    = [var.region]
 }
 
-resource "keycloak_openid_client" "my_app" {
-  realm_id  = keycloak_realm.apps.id
-  client_id = "my-app"
+resource "ovh_cloud_project_network_private_subnet" "subnet" {
+  service_name = var.ovh_project_id 
+  network_id   = ovh_cloud_project_network_private.network.id
 
-  name                  = "My App"
-  enabled               = true
-  access_type           = "CONFIDENTIAL"
-  standard_flow_enabled = true
-
-  valid_redirect_uris = [
-    "https://app.example.com/*"
-  ]
-
-  web_origins = [
-    "https://app.example.com"
-  ]
+  region     = var.region
+  start      = "192.168.168.100"
+  end        = "192.168.168.200"
+  network    = "192.168.168.0/24"
+  dhcp       = true
+  no_gateway = false
 }
 
-resource "keycloak_openid_client" "kube" {
-  realm_id  = keycloak_realm.apps.id
-  client_id = "user"
-  name      = "Kubernetes API"
-  access_type = "PUBLIC"
+resource "ovh_cloud_project_gateway" "gateway" {
+  service_name = var.ovh_project_id 
+  name       = "gateway"
+  model      = "s"
+  region     = var.region
+  network_id = tolist(ovh_cloud_project_network_private.network.regions_attributes[*].openstackid)[0]
+  subnet_id  = ovh_cloud_project_network_private_subnet.subnet.id
+}
 
-  standard_flow_enabled = true
-  valid_redirect_uris   = ["*"]
+data "template_file" "cloud_init" {
+  template = file("${path.module}/cloud-init-keycloak-caddy.yaml.tpl")
+
+  vars = {
+    KEYCLOAK_VERSION   = var.keycloak_version
+    KEYCLOAK_ADMIN     = var.keycloak_admin_user
+    KEYCLOAK_PASSWORD  = var.keycloak_admin_password
+    KEYCLOAK_HOSTNAME  = var.keycloak_hostname
+    LETSENCRYPT_EMAIL  = "admin@${replace(var.keycloak_hostname, "/^[^.]+\\./", "")}"
+  }
+}
+
+resource "ovh_cloud_project_instance" "keycloak_vm" {
+  service_name = var.ovh_project_id
+  region       = var.region
+  name         = "keycloak-vm"
+  
+  flavor {
+    flavor_id = var.instance_flavor
+  }
+
+  boot_from {
+    image_id = var.instance_image
+  }
+
+  ssh_key {
+    name = var.ssh_key_name_keycloack
+  }
+  billing_period = "hourly"
+  user_data       = data.template_file.cloud_init.rendered
+
+  network {
+    public = true
+  }
 }
