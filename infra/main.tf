8d199053eb2b2cb01eb143772ce182a547a78dff
Schoonderwalt,Rik R.R.P.J. van <I559307@git.fhict.nl>
2026-06-01 12:09:40 +0200

    feat: Uncomment DNS records to enable A-records for subdomains

--- infra/main.tf
+++ infra/main.tf
@@ -352,7 +352,7 @@ chat.<domain>   → Mattermost (Team Chat)
 meet.<domain>   → Jitsi Meet (Video Conferencing)
 =============================================================================
 */
-/* Temporary commented to remove errors until domains have been deliverd
+# Domains have been delivered.
 resource "ovh_domain_zone_record" "services" {
   for_each = var.subdomains
 
@@ -369,4 +369,3 @@ resource "ovh_domain_zone_record" "services" {
 data "ovh_domain_zone" "main" {
   name = var.domain
 }
-*/
